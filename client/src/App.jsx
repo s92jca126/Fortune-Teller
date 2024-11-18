333c9b0fdd226aae162ab5468db7e31c40580bd5
yunchi <yunchipang@gmail.com>
2024-11-17 17:30:14 -0800

    KAN-8: Create Input Form Component (#3)
    * feat: create input form component
    
    * feat: seperate form from main component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
+import "./App.css";
+import BirthForm from "./components/BirthForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [array, setArray] = useState([]);
-
-  const fetchAPI = async () => {
-    const response = await axios.get('http://127.0.0.1:8080/api/users');
-    console.log(response.data.users);
-    setArray(response.data.users);
-  };
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ul>
-          {array.map((user, index) => (
-            <li key={index}>{user}</li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Firtune Teller</h1>
+      </header>
+      <main>
+        <p>Please input your birth information below.</p>
+        <BirthForm />
+      </main>
+    </div>
   );
 }
 
