e43fa252b539d5c8e2236dd5124074236e1e03ee
yunchipang <yunchipang@gmail.com>
2024-11-25 11:40:24 -0800

    feat: add question field to input form

--- client/src/components/InputForm.test.js
+++ client/src/components/InputForm.test.js
@@ -1,15 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import BirthForm from "./BirthForm";
+import InputForm from "./InputForm";
 
-describe("BirthForm Component", () => {
+describe("InputForm Component", () => {
   it("renders all form fields and the submit button", () => {
-    render(<BirthForm />);
+    render(<InputForm />);
 
     // Check if all form fields are rendered
     expect(screen.getByLabelText(/Date of Birth:/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Time of Birth:/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Place of Birth:/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Gender:/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Question:/i)).toBeInTheDocument();
 
     // Check if the submit button is rendered
     expect(screen.getByText(/Submit/i)).toBeInTheDocument();
